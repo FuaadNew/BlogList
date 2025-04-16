31c096d068379e1dec08ab6903defa1441a43cd3
Fuaad <fuaadmohamoud@gmail.com>
2025-04-16 15:49:03 -0500

    Implement blog list tests with SuperTest
    - Add API tests for blog list endpoints
    - Verify blogs are returned as JSON
    - Test for correct number of blogs
    - Refactor route handlers to use async/await syntax

--- index.js
+++ index.js
@@ -6,34 +6,28 @@ const mongoose = require('mongoose')
 const app = express()
 
 app.use(express.json())
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-
-const blogItem = new Blog({
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-})
-
-
-blogItem.save().then((result) => {
-    console.log(result)
+app.get('/api/blogs', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
 
 
-app.post('/api/blogs', (request, response) => {
+
+//blogItem.save().then((result) => {
+   //console.log(blogItem)
+//})
+
+
+
+app.post('/api/blogs', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  await blog.save()
+  response.status(201).json(blog)
 })
+
+
 
 
 
@@ -41,3 +35,5 @@ const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+module.exports = app
