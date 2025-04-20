967430a3cf3e353ff2bfe220cf0be7e358af78a4
Fuaad <fuaadmohamoud@gmail.com>
2025-04-20 16:01:14 -0500

    wrote test that makes an HTTP GET request to the /api/blogs URL and verified that the blog list application returns the correct amount of blog posts in the JSON format

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -1,17 +1,31 @@
-const { test, after } = require('node:test')
+const { test, after} = require('node:test')
+const assert = require('node:assert')
+
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../index')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
+test('Blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
+
+test('Blogs length is 8', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .catch(error =>{
+      console.log(error)
+    })
+  
+  assert.strictEqual(response.body.length,8)
+  
+    
+})
 
 
 test(async () => {
