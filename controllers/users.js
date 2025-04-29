4ee145c1861058e265f3564efff4e62cfd9d9ce8
Fuaad <fuaadmohamoud@gmail.com>
2025-04-29 15:35:32 -0500

    Add controller-level validation for minimum password length
    - Ensure passwords are at least 3 characters long before user creation
    - Return a 400 error if the password does not meet the length requirement

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,11 @@ const User = require('../models/users.js')
 
 usersRouter.post('/',async(request,response)=>{
 const {username, name, password} = request.body
+
+if  (password.length < 3) {
+    response.status(400).json({"error":"password must be atleast 3 characters long"}).end()
+    return
+}
 const saltRounds = 10
 
 const passwordHash = await bcrypt.hash(password,saltRounds)
